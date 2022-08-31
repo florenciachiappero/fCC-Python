f10f32dd8e3a988fdf7f134a355e395d92ae4a2a
florenciachiappero <chiapperoflorencia@gmail.com>
2022-08-31 10:31:20 -0300

    Add Time Calculator project

--- Time_Calculator/time_calculator.py
+++ Time_Calculator/time_calculator.py
@@ -1,5 +1,85 @@
 def add_time(start, duration):
 
+    weekDays = {
+        "Sunday": 0,
+        "Monday": 1,
+        "Tuesday": 2,
+        "Wednesday": 3,
+        "Thursday": 4,
+        "Friday": 5,
+        "Saturday": 6
+    }
+
+
+    # Getting data from start string
+    startHour, midday = start.split()
+    hour, minutes = startHour.split(":")
+    hour = int(hour)
+    minutes = int(minutes) 
+
+    # 24 hour format 
+    if midday == "PM":
+        hour += 12
+
+
+    # Getting data from duration string
+    durationHour, durationMinutes = duration.split(":")
+    durationHour = int(durationHour)
+    durationMinutes = int(durationMinutes)
+
+
+    # Calculate total minutes
+    totalMinutes = minutes + durationMinutes
+    newMinutes = int(totalMinutes % 60)
+    if newMinutes < 10:
+        newMinutes = '0' + str(newMinutes)
+    else: 
+        newMinutes = str(newMinutes)
+
+    # Calculate hours
+    plusHours = totalMinutes / 60
+    totalHours = hour + durationHour + plusHours
+    newHours = int((totalHours % 24) % 12)
+    if newHours == 0:
+        newHours = 12
+    newHours = str(newHours)
+
+
+    # Calculate days
+    totalDays = int(totalHours / 24)
+
+    
+    # Get wether AM or PM
+    newMidday = ""
+    if (totalHours % 24) < 12:
+        newMidday = "AM"
+    else: 
+        newMidday = "PM"
+        
+    
+    # Return newTime
+    newTime = newHours + ":" + newMinutes + " " + newMidday
+    if day == None:
+        if totalDays == 0: 
+            return newTime
+        if totalDays == 1: 
+            return newTime + " (next day)"
+        else:
+            return newTime + " (" + str(totalDays) + " days later)"
+    
+    else:
+      newDay = (weekDays[day.lower().capitalize()] + totalDays) % 7
+      for i, j in weekDays.items():
+          if j == newDay:
+              newDay = i
+              break
+
+      if totalDays == 0:
+          return newTime + ', ' + newDay
+      if totalDays == 1:
+          return newTime + ', ' + newDay + ' (next day)'
+      return newTime + ', ' + newDay + ' (' + str(totalDays) + ' days later)'
+    
 
 
 
---
+++ Time_Calculator/try.py
@@ -0,0 +1,103 @@
+def main():
+
+    print(add_time("11:06 PM", "2:02"))
+    print(add_time("11:30 AM", "2:32", "monday"))
+    print(add_time("10:10 PM", "3:30"))
+    print(add_time("11:43 PM", "24:20", "tueSday"))
+    print(add_time("6:30 PM", "205:12"))
+    print(add_time("8:16 PM", "466:02"))
+    print(add_time("8:16 PM", "466:02", "tuesday"))
+
+
+
+def add_time(start, duration, day=None):
+
+    weekDays = {
+        "Sunday": 0,
+        "Monday": 1,
+        "Tuesday": 2,
+        "Wednesday": 3,
+        "Thursday": 4,
+        "Friday": 5,
+        "Saturday": 6
+    }
+
+
+    # Getting data from start string
+    startHour, midday = start.split()
+    hour, minutes = startHour.split(":")
+    hour = int(hour)
+    minutes = int(minutes) 
+
+    # 24 hour format 
+    if midday == "PM":
+        hour += 12
+
+
+    # Getting data from duration string
+    durationHour, durationMinutes = duration.split(":")
+    durationHour = int(durationHour)
+    durationMinutes = int(durationMinutes)
+
+
+    # Calculate total minutes
+    totalMinutes = minutes + durationMinutes
+    newMinutes = int(totalMinutes % 60)
+    if newMinutes < 10:
+        newMinutes = '0' + str(newMinutes)
+    else: 
+        newMinutes = str(newMinutes)
+
+    # Calculate hours
+    plusHours = totalMinutes / 60
+    totalHours = hour + durationHour + plusHours
+    newHours = int((totalHours % 24) % 12)
+    if newHours == 0:
+        newHours = 12
+    newHours = str(newHours)
+
+
+    # Calculate days
+    totalDays = int(totalHours / 24)
+
+    
+    # Get wether AM or PM
+    newMidday = ""
+    if (totalHours % 24) < 12:
+        newMidday = "AM"
+    else: 
+        newMidday = "PM"
+        
+    
+    # Return newTime
+    newTime = newHours + ":" + newMinutes + " " + newMidday
+    if day == None:
+        if totalDays == 0: 
+            return newTime
+        if totalDays == 1: 
+            return newTime + " (next day)"
+        else:
+            return newTime + " (" + str(totalDays) + " days later)"
+    
+    else:
+      newDay = (weekDays[day.lower().capitalize()] + totalDays) % 7
+      for i, j in weekDays.items():
+          if j == newDay:
+              newDay = i
+              break
+
+      if totalDays == 0:
+          return newTime + ', ' + newDay
+      if totalDays == 1:
+          return newTime + ', ' + newDay + ' (next day)'
+      return newTime + ', ' + newDay + ' (' + str(totalDays) + ' days later)'
+
+if __name__ == "__main__":
+    main()
+
+
+
+    
+    
+
+    
